53122df35443c8ef8884426526aa3eaf2fbb3cee
zachwood0s <zach@hayzak.com>
2019-03-19 18:51:43 -0500

    first commit

---
+++ GameServer/CardReader.cs
@@ -0,0 +1,85 @@
+﻿using HidLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GameServer
+{
+    public class CardReader: IDisposable
+    {
+        public const int VENDOR_ID = 0x0801;
+        public const int PRODUCT_ID = 0x0003;
+        private const int MAX_COMMAND_LEN = 64;
+
+        private static class Commands
+        {
+            public const byte ESCAPE = 0x1B;
+            public const byte READ = 0x72;
+            public const byte WRITE = 0x77;
+        }
+
+        private HidDevice _device;
+
+        public bool Connect()
+        {
+            var device = HidDevices.Enumerate(VENDOR_ID, PRODUCT_ID).FirstOrDefault();
+            if (device == null) return false;
+
+            _device = device;
+
+            _device.OpenDevice();
+
+            _device.Inserted += _DeviceAttachedHandler;
+            _device.Removed += _DeviceRemovedHandler;
+
+            _device.MonitorDeviceEvents = true;
+
+            return true;
+        }
+
+        public HidReport Read()
+        {
+            return _SendCommandWait(0xC5, Commands.ESCAPE, Commands.READ);
+        }
+
+        private void _SendCommand(params byte[] command)
+        {
+            _device.WriteFeatureData(_ExtendCommand(command.Prepend((byte) 0x00).ToArray()));
+        }
+
+        private HidReport _SendCommandWait(params byte[] command)
+        {
+            _SendCommand(command);
+            var report = _device.ReadReport();
+            return report;
+        }
+
+        private static byte[] _ExtendCommand(params byte[] command)
+            => command.Concat(Enumerable.Repeat<byte>(0x00, MAX_COMMAND_LEN - command.Length)).ToArray();
+
+        private void _DeviceAttachedHandler()
+        {
+            Console.WriteLine("device attached");
+        }
+
+        private void _DeviceRemovedHandler()
+        {
+            Console.WriteLine("removed");
+        }
+
+        public void Dispose()
+        {
+            if(_device == null)
+            {
+                Console.WriteLine("No device was ever opened! Did you forget to call Connect()?");
+            }
+            else
+            {
+                _device.CloseDevice();
+            }
+        }
+    }
+}
---
+++ GameServer/Program.cs
@@ -0,0 +1,62 @@
+﻿using HidLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GameServer
+{
+    class Program
+    {
+        private const int VendorId = 0x0801;
+        private const int ProductId = 0x0003;
+        private static HidDevice _device;
+
+        private static byte ESCAPE = 0x1B;
+        private static byte OFF_LED = 0x81;
+        private static byte GREEN_LED = 0x83;
+        private static byte YELLOW_LED = 0x84;
+        private static byte RED_LED = 0x85;
+        private static byte WRITE = 0x72;
+        static void Main(string[] args)
+        {
+            using(var reader = new CardReader())
+            {
+                if (reader.Connect())
+                {
+                    var report = reader.Read();
+                    Console.WriteLine(Encoding.ASCII.GetString(report.Data));
+                }
+            }
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+        
+        private static void Read()
+        {
+            _device.WriteFeatureData(extendCommand(0x00, 0xc5, ESCAPE, WRITE));
+            _device.ReadReport(OnReport);
+        }
+
+        private static byte[] extendCommand(params byte[] command)
+            => command.Concat(Enumerable.Repeat<byte>(0x00, 64 - command.Length)).ToArray();
+
+        private static void OnReport(HidReport report)
+        {
+            Console.WriteLine(report.Data);
+        }
+
+        private static void DeviceAttachedHandler()
+        {
+            Console.WriteLine("device attached");
+            _device.ReadReport(OnReport);
+        }
+
+        private static void DeviceRemovedHandler()
+        {
+            Console.WriteLine("removed");
+        }
+    }
+}
