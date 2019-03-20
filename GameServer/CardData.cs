719b53b7d2f8bd4545e923c7d659a178b5638f21
zachwood0s <zach@hayzak.com>
2019-03-19 20:17:12 -0500

    Update CardData.cs
    Added card data

--- GameServer/CardData.cs
+++ GameServer/CardData.cs
@@ -8,5 +8,33 @@ namespace GameServer
 {
     public class CardData
     {
+        private CardData()
+        {
+        }
+
+        public static CardData Create(string cardData)
+        {
+            var data = new CardData();
+            //Do conversion
+            return data;
+        }
+
+        public static CardData Create(HidReport report)
+        {
+            var data = new CardData();
+            // convert
+            return data;
+        }
+
+        public byte[] GetWritableData()
+        {
+            return new byte[0];
+        }
+
+        public string GetReadableData()
+        {
+            return "";
+        }
+        
     }
 }
